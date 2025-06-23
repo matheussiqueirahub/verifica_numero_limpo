f390df2d3a66323ff267850f64ca2b7675e9ccab
Matheus Siqueira <matheussiqueira@Mac-mini-de-Matheus.local>
2025-06-23 20:30:49 -0300

    Script para verificar se número é positivo, negativo ou zero

---
+++ positivozeronegativo.py
@@ -0,0 +1,9 @@
+numero = float(input("Digite um número: "))
+
+if numero > 0:
+    print(f"O número {numero} é positivo.")
+elif numero == 0:
+    print(f"O número é zero.")
+else:
+    print(f"O número {numero} é negativo.")
+    
